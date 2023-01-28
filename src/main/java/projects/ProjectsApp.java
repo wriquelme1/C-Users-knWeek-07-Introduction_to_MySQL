2ffee27cbea7785cd5c2ba354de07499f2bc68c4
wriquelme1 <wriquelme@gmail.com>
2023-01-28 17:45:24 -0500

    week10

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,6 +1,7 @@
 package projects;
 
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
@@ -8,117 +9,150 @@ import projects.exception.DbException;
 import projects.service.ProjectsService;
 import projects.entity.Project;
 
-
-
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectsService projectService = new ProjectsService();
-
+	private Project curProject;
+	
 //@formatter:off
- private List<String> operation = List.of("1) Add a project");
+ private List<String> operation = List.of(
+		 "1) Add a project",
+		 "2) List project",
+		 "3) Select a project");
  // @formatter:on
-
 
 	public static void main(String[] args) {
 		new ProjectsApp().processUserSelections();
 
 	}
 
-private void processUserSelections() {
-	boolean done = false;
+	private void processUserSelections() {
+		boolean done = false;
 		while (!done) {
 			try {
 				int selection = getUserSelection();
 				switch (selection) {
 				case -1:
-				done = exitMenu();
-				break;
+					done = exitMenu();
+					break;
 				case 1:
 					createProject();
 					break;
-					default:
-						System.out.println("-n"+ selection + " is not a valid selection. Try again.");
-						break;
+				case 2:
+					listProject();
+					break;
+				case 3:
+					selectProject();
+					break;
+				default:
+					System.out.println("-n" + selection + " is not a valid selection. Try again.");
+					break;
 				}
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				System.out.println("\nError: " + e + " Try again.");
 			}
-	       } 
-          }
+		}
+	}
 
-
-
-private void createProject() {
-	String projectName = getStringInput("Enter the project name");
-	BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
-	BigDecimal actualHours = getDecimalInput("Enter the actual hours");
-	Integer difficulty = getIntInput ("Enter the project difficulty (1-5)");
-	String notes = getStringInput("Enter the project notes");
-	
-	Project project = new Project();
-	
-	project.setProjectName(projectName);
-	project.setEstimatedHours(estimatedHours);
-	project.setActualHours(actualHours);
-	project.setDifficulty(difficulty);
-	project.setNotes(notes);
-	
-	Project dbProject = projectService.addProject(project);
-	System.out.println("You have successfully created project: " + dbProject);
-}
-/**
- * Gets the user's input from the console and converts it to a BigDecimal.
- * 
- * @param prompt the prompt to display on the console.
- * @return A BigDecimal value if successful.
- * @throws DbException thrown if an error occurs convertong the number to a BigDecimal.
- * */
-private BigDecimal getDecimalInput (String prompt) {
-	String input = getStringInput(prompt);
-	
-	if (Objects.isNull(input)) {
-		return null;
+private void selectProject() {
+		listProject();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId);
+		
 		
 	}
-	try {
-		return new BigDecimal(input).setScale(2);
-	}
-	catch(NumberFormatException e) {
-		throw new DbException(input + " is not a valid decimal number.");
-	}
-}
-private boolean exitMenu() {
-	System.out.println("Exiting the menu.");
-	return true;
-}
-private int getUserSelection() {
-	printOperations();
-	Integer input = getIntInput("Enter a menu selection");
-	return Objects.isNull(input) ? -1 : input;
-}
-private Integer getIntInput(String prompt) {
-	String input = getStringInput(prompt);
+
+private void listProject() {
+	List <Project> projects = projectService.fetchAllProjects();
+	System.out.println("\nProjects: ");
+	projects.forEach(project -> System.out.println("  "+project.getProjectId()+ ": "+ project.getProjectName()));
 	
-	if(Objects.isNull(input)) {
-		return null;
-	}
-	try {
-		return Integer.valueOf(input);
-	}
-	catch(NumberFormatException e) {
-		throw new DbException(input + " is not a valid number.");
-	}
-}
-private String getStringInput(String prompt) {
-	System.out.println(prompt + ": ");
-	String input = scanner.nextLine();
-	return input.isBlank() ? null : input.trim();
 }
 
-private void printOperations() {
-	System.out.println("\nThese are the available selections. Press the Enter key to quit:");
-	
-	operation.forEach(line -> System.out.println("  " + line));
-}
+	private void createProject() throws SQLException {
+		String projectName = getStringInput("Enter the project name");
+		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
+		BigDecimal actualHours = getDecimalInput("Enter the actual hours");
+		Integer difficulty = getIntInput("Enter the project difficulty (1-5)");
+		String notes = getStringInput("Enter the project notes");
+
+		Project project = new Project();
+
+		project.setProjectName(projectName);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+
+		Project dbProject = projectService.addProject(project);
+		System.out.println("You have successfully created project: " + dbProject);
+	}
+
+	/**
+	 * Gets the user's input from the console and converts it to a BigDecimal.
+	 * 
+	 * @param prompt the prompt to display on the console.
+	 * @return A BigDecimal value if successful.
+	 * @throws DbException thrown if an error occurs convertong the number to a
+	 *                     BigDecimal.
+	 */
+	private BigDecimal getDecimalInput(String prompt) {
+		String input = getStringInput(prompt);
+
+		if (Objects.isNull(input)) {
+			return null;
+
+		}
+		try {
+			return new BigDecimal(input).setScale(2);
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number.");
+		}
+	}
+
+	private boolean exitMenu() {
+		System.out.println("Exiting the menu.");
+		return true;
+	}
+
+	private int getUserSelection() {
+		printOperations();
+		Integer input = getIntInput("Enter a menu selection");
+		return Objects.isNull(input) ? -1 : input;
+	}
+
+	private Integer getIntInput(String prompt) {
+		String input = getStringInput(prompt);
+
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		try {
+			return Integer.valueOf(input);
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number.");
+		}
+	}
+
+	private String getStringInput(String prompt) {
+		System.out.println(prompt + ": ");
+		String input = scanner.nextLine();
+		return input.isBlank() ? null : input.trim();
+	}
+
+	private void printOperations() {
+		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
+
+		operation.forEach(line -> System.out.println("  " + line));
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with a project: "+curProject);
+		}
+	}
 }
--- src/main/java/projects/dao/ProjectDao.java
+++ src/main/java/projects/dao/ProjectDao.java
@@ -3,21 +3,30 @@ package projects.dao;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import projects.entity.Category;
+import projects.entity.Material;
 import projects.entity.Project;
+import projects.entity.Step;
 import projects.exception.DbException;
 import provided.util.DaoBase;
 
+@SuppressWarnings("unused")
 public class ProjectDao extends DaoBase {
-	
+
 	private static final String CATEGORY_TABLE = "category";
 	private static final String MATERIAL_TABLE = "material";
 	private static final String PROJECT_TABLE = "project";
 	private static final String PROJECT_CATEGORY_TABLE = "project_category";
 	private static final String STEP_TABLE = "step";
-	
-	
-public Project inertProject(Project project) {
+
+	public Project insertProject(Project project) throws SQLException {
 	// @formatter:off
 	String sql = ""
 			+"INSERT INTO " + PROJECT_TABLE + " "
@@ -25,34 +34,152 @@ public Project inertProject(Project project) {
 			+ "VALUES "
 			+ "(?, ?, ?, ?, ?)";
 	// @formatter:on
+		try (Connection conn = DbConnection.getConnection()) {
+	
+			startTransaction(conn);
+
+			try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+				setParameter(stmt, 1, project.getProjectName(), String.class);
+				setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
+				setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
+				setParameter(stmt, 4, project.getDifficulty(), Integer.class);
+				setParameter(stmt, 5, project.getNotes(), String.class);
+
+				stmt.executeUpdate();
+				
+				Integer projectId = getLastInsertId(conn, PROJECT_TABLE);
+				commitTransaction(conn);
+
+				project.setProjectId(projectId);
+				return project;
+			} 
+			catch (Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException (e);
+		}
+	}
+
+	/*public Project insertProject(Project project) {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+
+	public List<Project> fetchAllProject() {
+
+		String sql = "SELECT * FROM " + PROJECT_TABLE + " ORDER BY project_name";
+
+		try (Connection conn = DbConnection.getConnection()) {
+			startTransaction(conn);
+			try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+				try (ResultSet rs = stmt.executeQuery()) {
+					List<Project> project = new LinkedList<>();
+
+					while (rs.next()) {
+						project.add(extract(rs, Project.class));
+					}
+					return project;
+				}
+			} catch (Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+	}
+
+public Optional <Project> fetchProjectById(Integer projectId) {
+	
+	String sql = "SELECT * FROM " + PROJECT_TABLE + " WHERE project_id = ?";
+	
 	try(Connection conn = DbConnection.getConnection()){
 		startTransaction(conn);
-		
-		try(PreparedStatement stmt = conn.prepareStatement(sql)){
-			setParameter(stmt, 1, project.getProjectName(), String.class);
-			setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
-			setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
-			setParameter(stmt, 4, project.getDifficulty(), Integer.class);
-			setParameter(stmt, 5, project.getNotes(), String.class);
+		try {
+			Project project = null;
+			try(PreparedStatement stmt = conn.prepareStatement(sql)){
+				setParameter(stmt, 1, projectId, Integer.class);
+				try(ResultSet rs = stmt.executeQuery()){
+					if(rs.next()) {
+						project = extract (rs, Project.class);
+					}
+				}
+			}
 			
-			stmt.executeUpdate();
-			Integer projectId = getLastInsertId(conn, PROJECT_TABLE);
+			if(Objects.nonNull(project)) {
+				project.getMaterials().addAll(fetchMaterialsForProject(conn, projectId));
+				project.getSteps().addAll(fetchStepsForProject(conn, projectId));
+				project.getCategories().addAll(fetchCategoriessForProject(conn, projectId));
+			}
+			
 			commitTransaction(conn);
-			
-			project.setProjectId(projectId);
-			return project;
+			return Optional.ofNullable(project);
 		}
 		catch(Exception e) {
 			rollbackTransaction(conn);
 			throw new DbException(e);
 		}
 	}
-	
+	catch(SQLException e) {
+		throw new DbException(e);
+	}
 }
 
+private List<Category> fetchCategoriessForProject(Connection conn, Integer projectId) throws SQLException {
+String sql = "SELECT * FROM" + CATEGORY_TABLE + "WHERE project_id = ?";
+	
+	try(PreparedStatement stmt = conn.prepareStatement(sql)){
+		setParameter (stmt, 1, projectId, Integer.class);
+		
+		try(ResultSet rs = stmt.executeQuery()){
+			List<Category> categories = new LinkedList<>();
+			
+			while(rs.next()) {
+				categories.add(extract(rs, Category.class));
+			}
+			return categories;
+		}
+	}
+}
 
-public Project insertProject(Project project) {
-	// TODO Auto-generated method stub
-	return null;
+private List<Step> fetchStepsForProject(Connection conn, Integer projectId) throws SQLException {
+
+	String sql = "SELECT * FROM " + STEP_TABLE + " WHERE project_id =?";
+	
+	try(PreparedStatement stmt = conn.prepareStatement(sql)){
+		setParameter (stmt, 1, projectId, Integer.class);
+		
+		try(ResultSet rs = stmt.executeQuery()){
+			List<Step> steps = new LinkedList<>();
+			
+			while(rs.next()) {
+				steps.add(extract(rs, Step.class));
+			}
+			return steps;
+		}
+	}
+	
+	}
+
+private List<Material> fetchMaterialsForProject(Connection conn, Integer projectId) throws SQLException{
+	
+	String sql = "SELECT * FROM" + MATERIAL_TABLE + "WHERE project_id = ?";
+	
+	try(PreparedStatement stmt = conn.prepareStatement(sql)){
+		setParameter (stmt, 1, projectId, Integer.class);
+		
+		try(ResultSet rs = stmt.executeQuery()){
+			List<Material> materials = new LinkedList<>();
+			
+			while(rs.next()) {
+				materials.add(extract(rs, Material.class));
+			}
+			return materials;
+		}
+	}
+	
 }
 }
